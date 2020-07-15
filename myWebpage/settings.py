4ce5cdcadb1147d948c19f5c582efb8c4b4bfaf8
empatte2 <empatte2@ncsu.edu>
2020-07-15 11:04:49 -0400

    deploy

--- myWebpage/settings.py
+++ myWebpage/settings.py
@@ -11,22 +11,34 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+if os.envrion["DJANGO_DEBUG"] == "True":
+    DEBUG = True
+else:
+    DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1!wu0w(bw14pkd4cy!x$ixe9gz)3+_tvtk6va#7&!*o-fw^5q*'
+if DEBUG == True:
+    ENVVars = open(os.getcwd() + '/myWebpage/PreEnvVariables.txt', "r")
+    Var_List = []
+    for x in ENVVars:
+        Var_List.append(x.strip("\n"))
+    SECRET_KEY = Var_List[0]
+else:
+    SECRET_KEY = os.environ['secret_key']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
+# for allowed hosts
+if DEBUG == True:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ["evanpatterson.com", "23.92.30.30"]
 
 # Application definition
 
@@ -37,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home' # start of user apps
+    'home'  # start of user apps
 ]
 
 MIDDLEWARE = [
@@ -70,17 +82,39 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myWebpage.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
+    if 'test' in sys.argv:
+        DATABASES['default'] = {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'mydatabase'
+        }
+    else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': os.environ['db_name'],
+                'USER': os.environ['db_user'],
+                'PASSWORD': os.environ['db_password'],
+                'HOST': os.environ['db_location'],
+                'PORT': os.environ['db_port'],
+
+            }
+        }
+
+    if 'test' in sys.argv:
+        DATABASES['default'] = {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'mydatabase'
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -100,13 +134,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -114,14 +147,24 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'home/static')
+    os.path.join(BASE_DIR, 'home/static'),
 ]
+STATIC_ROOT=os.path.join('static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if DEBUG == False:
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 36000
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    X_FRAME_OPTIONS = 'DENY'
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
